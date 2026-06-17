dc30912af7d5f7b2d699bb50d4a7fe9d01a6b764
Chandni14 <chandni958016@gmail.com>
2026-06-17 13:54:58 +0530

    Chapter_06 and Chapter_6_PS

---
+++ Chapter_06/03_Quick_Quiz.py
@@ -0,0 +1,11 @@
+# A program which will print yes if the age entered by the user is greater than or equal to 18
+a = int(input("Enter your age: "))
+if(a>=18):
+    print("you are above the age of consent")
+    print("yes")
+else:
+    print("you are below the age of consent")    
+    print("no")
+    
+
+    
---
+++ Chapter_6_PS/05_problem5.py
@@ -0,0 +1,9 @@
+l = ["Harry", "Chandni", "Divya", "Shubh"]
+
+name = input("Enter your name:")
+
+if(name in l):
+    print("your name is in the list")
+
+else:
+    print("your name is not in the list")
